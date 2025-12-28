2c882fd7729bff118e4d794441baaf854b9cba7d
Kabilan K <kkabilan2024@gmail.com>
2025-12-28 12:05:00 +0530

    Added 404 Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import UserPage from './pages/UserPage';
 import ReportPage from './pages/ReportPage';
 import AdminPage from './pages/AdminPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
       <Route path="/user" element={<UserPage />} />
       <Route path="/user/report" element={<ReportPage />} />
       <Route path="/admin" element={<AdminPage />} />
-      <Route path="*" element={<div>Route not found</div>} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 };
